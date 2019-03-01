d64a89f553fab41c88a60bcefb125002bd758ff9
David Bolin <david@bolin.at>
2019-03-01 10:15:58 -0800

    finished third exercise

---
+++ dog-app-3/index.js
@@ -0,0 +1,49 @@
+'use strict';
+const store = {
+  currentNumber: 0,
+  urls: [],
+  invalid: false
+};
+
+function oneImgHtml(url) {
+  return `<img src="${url}" alt="random dog pic">`;
+}
+
+function render(){
+  if (!store.invalid) { 
+    let htmlString = '';
+    for (let i = 0; i < store.urls.length; i++) {
+      htmlString += oneImgHtml(store.urls[i]);
+    }
+    $('.dog-images').html(htmlString);
+  } else {
+    $('.dog-images').html('<p>Breed not found</p>');
+  }
+}
+
+function getURL(apiResult) {
+  if (apiResult.message === 'Breed not found') {
+    store.invalid = true;
+    store.urls = [];
+  } else {
+    store.urls = [apiResult.message];
+    store.invalid = false;
+  }
+  render();
+}
+
+function handleInput() {
+  $('button').on('click', function(event){
+    event.preventDefault();
+    const breed = $('#dog-breed').val();
+    store.currentNumber = 1;
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`).then(response => response.json()).then (val => getURL(val)).catch(e => e);
+  });
+}
+
+function main(){
+  handleInput();
+  render();
+}
+
+$(main);
